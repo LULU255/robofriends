3fc21a2c4c41e93b94c8b08ededad8fc02a59ca7
edc\LHepp <leonhard.hepp@edcom.de>
2022-10-11 10:32:17 +0200

    Updates

---
+++ precache-manifest.6aa10cdf2882d4e0ad96755a60e0e532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac32103fd23a6e7c8f28",
+    "url": "/robofriends/static/css/main.3c42fbff.chunk.css"
+  },
+  {
+    "revision": "ac32103fd23a6e7c8f28",
+    "url": "/robofriends/static/js/main.ac32103f.chunk.js"
+  },
+  {
+    "revision": "1390c39a0a60d8108949",
+    "url": "/robofriends/static/css/1.4e6ee276.chunk.css"
+  },
+  {
+    "revision": "1390c39a0a60d8108949",
+    "url": "/robofriends/static/js/1.1390c39a.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d3748d16489dced8450eb6acc40de01c",
+    "url": "/robofriends/index.html"
+  }
+];
